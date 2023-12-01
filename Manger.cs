22635442abaa3d113f3e8b79c5cada18d8a16f1f
2901shivam <shivamsingh201617@gmail.com>
2023-12-01 16:06:55 +0530

    Add project files.

---
+++ Manger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySharedLibrary;
+
+namespace Assignment24Day20
+{
+    public class Manger
+    {
+        string name;
+        string department;
+        public Manger(string name,string department)
+        {
+            this.name = name;
+            this.department = department;
+            
+        }
+
+
+        public void MangerDetails()
+        {
+            Console.WriteLine($"Welcome {name} of this {department}");
+
+          
+        }
+
+     
+       
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySharedLibrary;
+
+namespace Assignment24Day20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter manger name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter Department Name");
+            string depart = Console.ReadLine();
+
+            Manger manger = new Manger(name, depart);
+
+            manger.MangerDetails();
+
+            Log log = new Log(10, "abc");
+
+
+        }
+    }
+}
